05a049feb8d2670e0bac3fc49d0f5ff88091576f
Jose Luis Santiago <jlsantia@ucsc.edu>
2017-11-13 19:38:56 -0800

    Lab4.asm

--- lab4/Lab4.asm
+++ lab4/Lab4.asm
@@ -31,22 +31,23 @@
 	
 	Outer_Loop:
 	li	$t3,2
-	bgt  	$t2,$s1,leave #if $t2 is greater than $t0, branch to leave 
 	addi	$t2,$t2,1
+	bgt  	$t2,$s1,leave #if $t2 is greater than $t0, branch to leave 
 	j	Loop
 	nop
 	
 	#Loop that shifts and reads values in addresses
-	Loop: #fix here      v
+	Loop: 
+	bge	$t3,$t2,print #print if remainder is greater than zero
+	nop
 	rem 	$t1,$t2,$t3 #divide $t2 by $t3 and put the remainder back in $t1
 	beqz	$t1,Outer_Loop #branch if the remainder is zero
-	nop
-	bgtz	$t1,print #print if remainder is greater than zero
 	nop
 	addi	$t3,$t3,1 #increment inner loop counter
 	#beq	$t3,$t2,Outer_Loop #if my counter is equal to user input, leave
 	j	Loop
 	nop
+	
 	
 	#once Loop determines a number is prime it is sent here to print then looped back to loop
 	print:
@@ -58,7 +59,6 @@
 	move  	$a0,$t2 #move number in address at the time into a0
 	syscall
 	
-	addi	$t3,$t3,1 #increment inner loop counter
 	j	Outer_Loop
 	nop
 	
@@ -70,7 +70,7 @@
 
 .data
 	Comma:	.asciiz ","
-	Begin:	.asciiz "1,2,3"
+	Begin:	.asciiz "2,3"
 	Welcome:	.asciiz "Jose Luis Santiago\n CMPE-12\n11/13/17\nWelcome to my Program\n"
 	Prompt:	.asciiz "Please Input a Number:"
 	End_Prompt:	.asciiz "Output:"
